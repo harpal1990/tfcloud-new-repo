a1c19d659fb966c6c64240ab2ef899157b4aa477
harpal1990 <harpalgohilon@gmail.com>
2023-09-30 20:13:20 +0530

    update files for terraform cloud

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = var.instance_type
-  count                  = 2
+  count                  = 3
   key_name               = "aws"
   user_data              = file("${path.module}/install_nginx.sh")
   vpc_security_group_ids = ["${aws_security_group.TechVPC_SG.name}"]
